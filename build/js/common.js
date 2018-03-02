a0c46f73f2472c2d56af5ac274721760cc67e617
Evgeniy Selevko <evgeniyselevko@gmail.com>
2018-03-02 18:04:54 +0200

    add lib

--- build/js/common.js
+++ build/js/common.js
@@ -10,11 +10,11 @@ $(document).ready(function() {
 		);
 	$('.arr-left').click(function(){
   $('.reviews-slide').slick('slickPrev');
-})
+});
 
 $('.arr-right').click(function(){
   $('.reviews-slide').slick('slickNext');
-})
+});
 $('.clients-slide').slick({
 	 infinite: true,
   slidesToShow: 5,
@@ -24,9 +24,16 @@ $('.clients-slide').slick({
 });
 	$('.arr-left-client').click(function(){
   $('.clients-slide').slick('slickPrev');
-})
-
+});
 $('.arr-right-client').click(function(){
   $('.clients-slide').slick('slickNext');
-})
+});
+$(".spincrement").spincrement({
+    from: 0,                // Стартовое число
+    to: false,              // Итоговое число. Если false, то число будет браться из элемента с классом spincrement, также сюда можно напрямую прописать число. При этом оно может быть, как целым, так и с плавающей запятой
+    decimalPlaces: 0,       // Сколько знаков оставлять после запятой
+    decimalPoint: ".",      // Разделитель десятичной части числа
+    thousandSeparator: ",", // Разделитель тыcячных
+    duration: 10000          // Продолжительность анимации в миллисекундах
+});
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 //=include lib/jquery.js
 //=include lib/slick.min.js
+//=include ../../node_modules/wowjs/dist/wow.min.js
+//=include lib/jquery.spincrement.js
 //=include common.js
